ac43d59b470564d380fef92164063f5a536c6d8b
sken100627 <shigujianren01@gmail.com>
2023-09-17 23:14:16 +0900

    RSpec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 12 }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  
+
 end
