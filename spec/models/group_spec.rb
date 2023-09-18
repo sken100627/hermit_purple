77e7c684c195fe543ca2191f794c2dc0266a6f36
sken100627 <shigujianren01@gmail.com>
2023-09-18 17:48:06 +0900

    グループ作成モデル単体テストコード

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @group = FactoryBot.build(:group)
+  end
+
+  describe 'グループ新規作成' do
+    context 'グループ新規作成できるとき' do
+      it 'group_nameとintroduction、group_imageが存在すれば登録できる' do
+        expect(@group).to be_valid
+      end
+    end
+    context 'グループ新規作成できないとき' do
+      it 'group_nameが空では登録できない' do
+        @group.group_name = ''
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Group name can't be blank")
+      end
+      it 'introductionが空では登録できない' do
+        @group.introduction = ''
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Introduction can't be blank")
+      end
+      it 'group_imageが空では登録できない' do
+        @group.group_image = nil
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Group image can't be blank")
+      end
+    end
+  end
+
 end
