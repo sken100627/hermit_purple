72f46e610b5fb7b7ce36824a640a95b7b0f1fa42
sken100627 <shigujianren01@gmail.com>
2023-09-21 00:04:46 +0900

    items#newページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
   end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @item = Item.new
+  end
 end
