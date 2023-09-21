301f7c9f75cb1f3b7b5d7b18581fd86776f0a9f4
sken100627 <shigujianren01@gmail.com>
2023-09-22 01:00:35 +0900

    検索機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   belongs_to :group
   has_one_attached :item_image
   has_one_attached :storage_image
+
+  def self.search(search)
+    if search != ""
+      Item.where('name LIKE(?)', "%#{search}%")
+    else
+      Item.all
+    end
+  end
 end
