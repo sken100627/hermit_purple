5200661ef0c60fdb55d5ad08e47998d8508f7171
sken100627 <shigujianren01@gmail.com>
2023-09-22 11:29:25 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,12 @@ class Item < ApplicationRecord
   has_one_attached :item_image
   has_one_attached :storage_image
 
+  with_options presence: true do
+    validates :name
+    validates :item_image
+    validates :quantity
+  end
+
   def self.search(search)
     Item.where('name LIKE(?)', "%#{search}%")
   end
