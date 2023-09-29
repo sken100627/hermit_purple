9b6ff9de6f0ec72500dc99d2c1d5f934fcefd763
sken100627 <shigujianren01@gmail.com>
2023-09-29 18:52:03 +0900

    nickname一意制約バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
 
-  validates :nickname, presence: true, length: { maximum: 12, message: 'は12文字以内で登録してください' }
+  validates :nickname, presence: true, length: { maximum: 12, message: 'は12文字以内で登録してください' }, uniqueness: { message: 'は既に使用されています' }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で登録してください' }
 end
